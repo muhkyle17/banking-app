182555adcff8b644cbefe93c3682c0617c6c9b98
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-04-16 14:39:02 -0700

    Make login go to bank with submitted -- still needs functionality where user owner is same as pin

--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,70 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import logo from '../public/logo.png'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import { data } from 'autoprefixer'
+import Router from 'next/router'
 
 function login() {
+  const API = 'http://localhost:3000'
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm()
+  const [userNameList, setUserNameList] = useState([])
+  const [userPinList, setUserPinList] = useState([])
+  const [userFirstName, setUserFirstName] = useState('')
+  const [userPin, setUserPin] = useState('')
+  // const [errorList, setErrorList] = useState([])
+  const onSubmit = (data) => {
+    console.log(data)
+    setUserFirstName(data.username)
+    setUserPin(data.password)
+  }
+  // console.log(errors)
 
-  const onSubmit = (data) => console.log(data)
-  // console.log(watch('username'), watch('password'))
+  let userNameArray = []
+  let userPinArray = []
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `${API}/accounts/`,
+      // data: {
+      //   owner: data.username,
+      //   pin: data.password,
+      // },
+    })
+      .then((res) => {
+        data = res.data
+        console.log(data)
+        data.map((user) => {
+          const userPinString = user.pin.toString()
+          userPinArray.push(userPinString)
+          setUserPinList(userPinArray)
+
+          userNameArray.push(user.owner)
+          setUserNameList(userNameArray)
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .then(() => {})
+  }, [])
+
+  if (userNameList.includes(userFirstName) && userPinList.includes(userPin)) {
+    Router.push('/bank')
+  }
+
+  // if (userPinArray.includes(userPin)) {
+  //   console.log('This worked')
+  // } else {
+  //   console.log('Refactor the pin stuff')
+  // }
 
   return (
     <>
@@ -24,14 +75,24 @@ function login() {
             className='text-3xl rounded-xl px-5 pt-4 pb-3 outline-none text-black placeholder-gray-300'
             type='text'
             placeholder='Username'
-            {...register('username')}
+            {...register('username', { required: true })}
           />
+          {errors.username && (
+            <span className='px-5 text-2xl text-red-600'>
+              Please type in your username
+            </span>
+          )}
           <input
             className='text-3xl rounded-xl px-5 pt-4 pb-3 outline-none text-black placeholder-gray-300'
             type='password'
             placeholder='Password'
-            {...register('password')}
+            {...register('password', { required: true })}
           />
+          {errors.password && (
+            <span className='px-5 text-2xl text-red-600'>
+              Please type in your password
+            </span>
+          )}
           <button
             type='submit'
             className='font-extrabold text-white text-3xl px-5 pb-3 py-4 bg-[#2bb580] hover:bg-[#1b9264] rounded-xl'
